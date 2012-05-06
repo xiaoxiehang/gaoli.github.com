5ea14d13dc3ccc833baefe58fe02d65570dda140
gaoli <3071730@qq.com>
2012-05-06 12:36:31 +0800

    add js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+﻿/**
+ * @Description: script.js
+ * @Version:     v1.0
+ * @Author:      GaoLi
+ */
+
+/**
+ * 百度统计
+ */
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Faadb874c4f4df417cacd2a85753c677b' type='text/javascript'%3E%3C/script%3E"));
